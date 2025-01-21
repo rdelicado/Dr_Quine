1dab425b5abab6d9ebf958d5b3689554e05bb956
rdelicad <rdelicad@gmail.com>
2025-01-21 10:56:48 +0100

    rm

--- C/ex02/Grace.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#define CODE "#include <stdio.h>%1$c%1$c#define CODE %2$c%3$s%2$c%1$c%1$c#define GRACE FILE *f = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); fprintf(f, CODE, 10, 34, CODE); fclose(f); return (0);%1$c%1$c#define RUN /* This macro runs the program */ int main() { GRACE }%1$c%1$cRUN"
-
-#define GRACE FILE *f = fopen("Grace_kid.c", "w"); fprintf(f, CODE, 10, 34, CODE); fclose(f); return (0);
-
-#define RUN /* This macro runs the program */ int main() { GRACE }
-
-RUN
--- C/ex02/subject.md
+++
@@ -1,8 +0,0 @@
-• The executable must be named Grace.
-• When executed, the program writes in a file named Grace_kid.c/Grace_kid.s
-the source code of the file used to compile the program.
-• The source code must strickly contain
-◦ No main declared.
-◦ Three defines only.
-◦ One comment.
-• The program will run by calling a macro.

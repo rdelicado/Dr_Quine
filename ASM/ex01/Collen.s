58042805885947cb4d113a6627f5b0918d568d28
rdelicad <rdelicad@gmail.com>
2025-02-11 17:22:51 +0100

    Collen asm

--- ASM/ex01/Collen.s
+++ ASM/ex01/Collen.s
@@ -1,25 +1,30 @@
+; Quine in assembly
+
 section .data
-    code db "hola mundo", 0, 0xA    
-    len equ $ - code                ; Calcula la len del code
+    code db "; Quine in assembly%1$c%1$csection .data%1$c    code db %2$c%3$s%2$c%1$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c    extern fflush%1$c%1$c_start:%1$c    call argv%1$c    call printf%1$c%1$c    ; fflush(NULL) empty all buffers%1$c    mov edi, 0%1$c    call fflush%1$c%1$c    ; exit the program%1$c    mov rax, 60%1$c    xor rdi, rdi%1$c    syscall%1$c%1$cargv:%1$c    lea rdi, [code]%1$c    mov esi, 10%1$c    mov edx, 34%1$c    lea rcx, [code]%1$c    mov eax, 0%1$c    ret"
 
 section .text
-    global _start                   ; Punto de entrada del programa
+    global _start
+    extern printf
+    extern fflush
 
 _start:
-    ; Llamada al sistema para escribir (sys_write)
-    mov rax, 1                      ; Número de syscall para escribir (1: sys_write)
-    mov rdi, 1                      ; Descriptor de archivo (1: salida estándar)
-    mov rsi, code                   ; Dirección del code
-    mov rdx, len                    ; len del code
-    syscall                         ; Invoca la llamada al sistema
+    call argv
+    call printf
 
-    ; Llamada al sistema para salir (sys_exit)
-    mov rax, 60                     ; Número de syscall para salir (60: sys_exit)
-    xor rdi, rdi                    ; Código de salida (0: éxito)
-    syscall                         ; Invoca la llamada al sistema
+    ; fflush(NULL) empty all buffers
+    mov edi, 0
+    call fflush
 
+    ; exit the program
+    mov rax, 60
+    xor rdi, rdi
+    syscall
 
-; 0xA   salto de linea \n
-; 0x09  tabulador \t
-; 0x22  comillas dobles "
-; 0     caracter nulo '\0'
+argv:
+    lea rdi, [code]
+    mov esi, 10
+    mov edx, 34
+    lea rcx, [code]
+    mov eax, 0
+    ret

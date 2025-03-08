4eb7e1b5a5ec15aa0c05e04788f8a794156a6d68
rdelicad <rdelicad@gmail.com>
2025-03-08 19:53:22 +0100

    finish Collen.s

--- ASM/ex01/Collen.s
+++ ASM/ex01/Collen.s
@@ -1,30 +1,32 @@
 ; Quine in assembly
 
 section .data
-    code db "; Quine in assembly%1$c%1$csection .data%1$c    code db %2$c%3$s%2$c%1$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c    extern fflush%1$c%1$c_start:%1$c    call argv%1$c    call printf%1$c%1$c    ; fflush(NULL) empty all buffers%1$c    mov edi, 0%1$c    call fflush%1$c%1$c    ; exit the program%1$c    mov rax, 60%1$c    xor rdi, rdi%1$c    syscall%1$c%1$cargv:%1$c    lea rdi, [code]%1$c    mov esi, 10%1$c    mov edx, 34%1$c    lea rcx, [code]%1$c    mov eax, 0%1$c    ret"
+    code db "; Quine in assembly%1$c%1$csection .data%1$c    code db %2$c%3$s%2$c%1$c%1$csection .text%1$c    global _main%1$c    extern _printf%1$c    extern _fflush%1$c%1$c_main:%1$c    sub rsp, 8  ; Aling the stack to 16 bytes%1$c    call argv%1$c    call _printf%1$c    add rsp, 8  ; Restore the stack%1$c%1$c    ; fflush(NULL) empty all buffers%1$c    xor rdi, rdi%1$c    call _fflush%1$c%1$c    ; exit the program%1$c    mov rax, 0x2000001  ; macOS exit syscall%1$c    xor rdi, rdi%1$c    syscall%1$c%1$cargv:%1$c    lea rdi, [rel code]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel code]%1$c    xor rax, rax%1$c    ret"
 
 section .text
-    global _start
-    extern printf
-    extern fflush
+    global _main
+    extern _printf
+    extern _fflush
 
-_start:
+_main:
+    sub rsp, 8  ; Aling the stack to 16 bytes
     call argv
-    call printf
+    call _printf
+    add rsp, 8  ; Restore the stack
 
     ; fflush(NULL) empty all buffers
-    mov edi, 0
-    call fflush
+    xor rdi, rdi
+    call _fflush
 
     ; exit the program
-    mov rax, 60
+    mov rax, 0x2000001  ; macOS exit syscall
     xor rdi, rdi
     syscall
 
 argv:
-    lea rdi, [code]
-    mov esi, 10
-    mov edx, 34
-    lea rcx, [code]
-    mov eax, 0
+    lea rdi, [rel code]
+    mov rsi, 10
+    mov rdx, 34
+    lea rcx, [rel code]
+    xor rax, rax
     ret

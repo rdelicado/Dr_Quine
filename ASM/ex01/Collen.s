6b0d467aae2b960699010b2a5d554363f5554d98
Ruben Delicado Garcia-Villaraco <rdelicad@c3r7s1.42malaga.com>
2025-03-13 15:25:35 +0100

    ex01 modificado para linux 42

--- ASM/ex01/Collen.s
+++ ASM/ex01/Collen.s
@@ -1,25 +1,23 @@
 ; Quine in assembly
 
 section .data
-    code db "; Quine in assembly%1$c%1$csection .data%1$c    code db %2$c%3$s%2$c%1$c%1$csection .text%1$c    global _main%1$c    extern _printf%1$c    extern _fflush%1$c%1$c_main:%1$c    sub rsp, 8  ; Aling the stack to 16 bytes%1$c    call argv%1$c    call _printf%1$c    add rsp, 8  ; Restore the stack%1$c%1$c    ; fflush(NULL) empty all buffers%1$c    xor rdi, rdi%1$c    call _fflush%1$c%1$c    ; exit the program%1$c    mov rax, 0x2000001  ; macOS exit syscall%1$c    xor rdi, rdi%1$c    syscall%1$c%1$cargv:%1$c    lea rdi, [rel code]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel code]%1$c    xor rax, rax%1$c    ret"
+    code db "; Quine in assembly%1$c%1$csection .data%1$c    code db %2$c%3$s%2$c%1$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c    extern fflush%1$c%1$c_start:%1$c    call argv%1$c    call printf%1$c%1$c    ; fflush(NULL) empty all buffers%1$c    xor rdi, rdi%1$c    call fflush%1$c%1$c    ; exit the program%1$c    mov rax, 60%1$c    xor rdi, rdi%1$c    syscall%1$c%1$cargv:%1$c    lea rdi, [rel code]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel code]%1$c    xor rax, rax%1$c    ret"
 
 section .text
-    global _main
-    extern _printf
-    extern _fflush
+    global _start
+    extern printf
+    extern fflush
 
-_main:
-    sub rsp, 8  ; Aling the stack to 16 bytes
+_start:
     call argv
-    call _printf
-    add rsp, 8  ; Restore the stack
+    call printf
 
     ; fflush(NULL) empty all buffers
     xor rdi, rdi
-    call _fflush
+    call fflush
 
     ; exit the program
-    mov rax, 0x2000001  ; macOS exit syscall
+    mov rax, 60
     xor rdi, rdi
     syscall
 

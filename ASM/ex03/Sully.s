425f32da18bbe9ff04267d102101cf1b5711441e
rdelicad <rdelicad@c3r6s1.42malaga.com>
2025-04-04 19:21:19 +0200

    Sully

--- ASM/ex03/Sully.s
+++ ASM/ex03/Sully.s
@@ -11,7 +11,6 @@ section .data
     run_cmd db "./Sully_%d", 0      ; Run command
     mode db "w", 0
     file_handle dq 0                ; File handle
-    remove_eot db "sed -i '$ s/\x04//g' %s", 0
 
 section .text
     extern sprintf  ; For string formatting
